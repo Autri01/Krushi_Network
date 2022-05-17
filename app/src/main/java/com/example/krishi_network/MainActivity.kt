76f8d907d40698a0442a003ef4f19438c1c2bed2
Autri <autrininja@gmail.com>
2022-05-18 00:13:49 +0530

    forth commit

--- app/src/main/java/com/example/krishi_network/MainActivity.kt
+++ app/src/main/java/com/example/krishi_network/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.krishi_network
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -12,44 +14,11 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    val api_key = "AIzaSyCZZ8_YMUFPRTcvHTUZ-6Y2_brRlhiTjbk"
-    var videoIdx = arrayOf<String>("IEF6mw7eK4s", "8CEJoCr_9UI", "3-nM3yNi3wg","kJQP7kiw5Fk","pRpeEdMmmQ0","RgKAFK5djSk","yU0tnrEk8H4")
-    var list:List<String> = videoIdx.toList()
-    lateinit var youTubePlayerInit : YouTubePlayer.OnInitializedListener
-    lateinit var youTubePlayer: YouTubePlayer
-    val random = Random()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        youtubePlayerSetup()
-        btnPlay.setOnClickListener {
-            var idx = videoIdx[random.nextInt(videoIdx.size-1)]
-            youTubePlayer.cueVideo(videoIdx[random.nextInt(videoIdx.size-1)])
-        }
-
-    }
-
-    private fun youtubePlayerSetup() {
-        ytPlayer.initialize(api_key, object : YouTubePlayer.OnInitializedListener{
-            override fun onInitializationSuccess(
-                p0: YouTubePlayer.Provider?,
-                p1: YouTubePlayer?,
-                p2: Boolean
-            ) {
-                youTubePlayer= p1!!
-                p1.cueVideo(videoIdx[random.nextInt(videoIdx.size-1)])
-                p1?.play()
-            }
-
-            override fun onInitializationFailure(
-                p0: YouTubePlayer.Provider?,
-                p1: YouTubeInitializationResult?
-            ) {
-                Toast.makeText(applicationContext,"Error", Toast.LENGTH_SHORT).show()
-            }
-
-        })
+        Handler().postDelayed({
+            val intent = Intent(this, YoutubeActivity::class.java)
+            startActivity(intent)
+        }, 3000)
     }
 }
