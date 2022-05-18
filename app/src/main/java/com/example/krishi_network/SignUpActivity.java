dc0660bf30d7a5a3efdc7661c83d3bb96793279d
Autri <autrininja@gmail.com>
2022-05-18 08:57:42 +0530

    changes made

--- app/src/main/java/com/example/krishi_network/SignUpActivity.java
+++ app/src/main/java/com/example/krishi_network/SignUpActivity.java
@@ -2,10 +2,21 @@ package com.example.krishi_network;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Parcelable;
 import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.view.View;
@@ -16,11 +27,20 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 public class SignUpActivity extends AppCompatActivity {
 
+    public static final int REQUEST_ID_MULTIPLE_PERMISSIONS = 101;
     ImageView imgView;
     Button button3;
-    private static int PICK_IMAGE_REQUEST = 1;
+    //private static int PICK_IMAGE_REQUEST = 1;
     Uri imageUri;
     EditText etName, etEmail;
 
@@ -75,29 +95,36 @@ public class SignUpActivity extends AppCompatActivity {
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        try {
-
-            if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && null != data) {
-                // Get uri
-                imageUri = data.getData();
-
-                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
-                int nh = (int) (bitmap.getHeight() * (1024.0 / bitmap.getWidth()));
-                Bitmap scaled = Bitmap.createScaledBitmap(bitmap, 1024, nh, true);
-                // Set image
-                imgView.setImageBitmap(scaled);
-
-            } else {
-                Toast.makeText(this, "No.", Toast.LENGTH_LONG).show();
+        if (resultCode != RESULT_CANCELED) {
+            switch (requestCode) {
+                case 0:
+                    if (resultCode == RESULT_OK && data != null) {
+                        Bitmap selectedImage = (Bitmap) data.getExtras().get("data");
+                        imgView.setImageBitmap(selectedImage);
+                    }
+                    break;
+                case 1:
+                    if (resultCode == RESULT_OK && data != null) {
+                        Uri selectedImage = data.getData();
+                        String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                        if (selectedImage != null) {
+                            Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+                            if (cursor != null) {
+                                cursor.moveToFirst();
+                                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                                String picturePath = cursor.getString(columnIndex);
+                                imgView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+                                cursor.close();
+                            }
+                        }
+                    }
+                    break;
             }
-
-        } catch (Exception e) {
-            Toast.makeText(this, "Oops! Sorry", Toast.LENGTH_LONG).show();
-            e.printStackTrace();
+        }
         }
 
 
-    }
+
     public boolean checkRequiredFields() {
         if (!etName.getText().toString().isEmpty()) {
             return true;
@@ -107,8 +134,27 @@ public class SignUpActivity extends AppCompatActivity {
     }
 
     public void loadImagefromGallery(View view) {
-        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.setType("image/*");
-        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
-    }
+        final CharSequence[] optionsMenu = {"Take Photo", "Choose from Gallery", "Exit" }; // create a menuOption Array
+        // create a dialog for showing the optionsMenu
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        // set the items in builder
+        builder.setItems(optionsMenu, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if(optionsMenu[i].equals("Take Photo")){
+                    // Open the camera and get the photo
+                    Intent takePicture = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(takePicture, 0);
+                }
+                else if(optionsMenu[i].equals("Choose from Gallery")){
+                    // choose from  external storage
+                    Intent pickPhoto = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(pickPhoto , 1);
+                }
+                else if (optionsMenu[i].equals("Exit")) {
+                    dialogInterface.dismiss();
+                }
+            }
+        });
+        builder.show(); }
 }
--- app/src/main/java/com/example/krishi_network/YoutubeActivity.java
+++ app/src/main/java/com/example/krishi_network/YoutubeActivity.java
@@ -1,8 +1,10 @@
 package com.example.krishi_network;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.ActionBar;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
@@ -13,6 +15,8 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.gms.cast.VideoInfo;
 import com.google.android.youtube.player.YouTubeBaseActivity;
@@ -30,28 +34,37 @@ public class YoutubeActivity extends YouTubeBaseActivity {
     private com.google.android.youtube.player.YouTubePlayer mYouTubePlayer;
     private Button mButton,btnNext;
     Random random;
-    int vidIdx=0;
+    int vidIdx;
 
     private final String[] VIDEO_CODE={"IEF6mw7eK4s", "8CEJoCr_9UI", "3-nM3yNi3wg","kJQP7kiw5Fk","pRpeEdMmmQ0","RgKAFK5djSk","yU0tnrEk8H4"};
     private final String API_KEY="AIzaSyCZZ8_YMUFPRTcvHTUZ-6Y2_brRlhiTjbk";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 1);
+        }
         setContentView(R.layout.activity_youtube);
+
 
         youTubePlayerSetup();
         mButton  = findViewById(R.id.btnPlay);
         btnNext  = findViewById(R.id.btnNext);
         random=new Random();
 
+
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (vidIdx<VIDEO_CODE.length-1) {
-                    vidIdx++;
-                }else{
-                    vidIdx=0;
-                }
+               vidIdx=random.nextInt(VIDEO_CODE.length-1);
                 mYouTubePlayer.cueVideo(VIDEO_CODE[vidIdx]);
             }
         });
@@ -72,6 +85,7 @@ public class YoutubeActivity extends YouTubeBaseActivity {
             @Override
             public void onInitializationSuccess(com.google.android.youtube.player.YouTubePlayer.Provider provider, com.google.android.youtube.player.YouTubePlayer youTubePlayer, boolean b) {
                 if(!b){
+                    vidIdx=random.nextInt(VIDEO_CODE.length-1);
                     mYouTubePlayer =youTubePlayer;
                     mYouTubePlayer.cueVideo(VIDEO_CODE[vidIdx]);
                     mYouTubePlayer.play();
